a5ce20158514e6f06d3b0d221d18be36bbf2c6c7
Josiah deGrasse <josiahdegrasse@gmail.com>
2026-08-21 12:19:41 +0100

    Phase 10: remove generated artifacts and dead code

--- src/app/components/responses-workspace.tsx
+++
@@ -1,9 +0,0 @@
-import { AdminConfig } from './admin-config';
-
-export function ResponsesWorkspace() {
-  return (
-    <div className="space-y-6">
-      <AdminConfig mode="responses" />
-    </div>
-  );
-}
